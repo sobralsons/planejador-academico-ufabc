a27f9aed0e72d84808d858e13943ba159196a595
Lucas Sobral <131934489+sobralsons@users.noreply.github.com>
2026-09-16 12:21:51 -0300

    Atualiza resumo global após lote 5

--- tests/test_auditoria_aplicabilidade_lote_04.py
+++ tests/test_auditoria_aplicabilidade_lote_04.py
@@ -135,9 +135,9 @@ def test_lote_04_altera_resumo_global_em_exatamente_uma_matriz_sem_criar_exclusa
     assert vigencia["resumo"] == {
         "ppcs_total": 93,
         "nao_aplicaveis": 0,
-        "pendentes": 46,
-        "candidatas": 47,
-        "ppcs_historicos_candidatos": 12,
+        "pendentes": 45,
+        "candidatas": 48,
+        "ppcs_historicos_candidatos": 13,
     }
 
 
